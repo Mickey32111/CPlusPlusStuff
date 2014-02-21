d6c89ae005e4cd9e69200289f6782e0fe5eba07d
Michael Smith <smithmichael130@gmail.com>
2014-02-21 20:46:04 +0000

    Updated to show user values

--- arithmetic/arithmetic.cpp
+++ arithmetic/arithmetic.cpp
@@ -4,7 +4,8 @@ using namespace std ;
 int main()
 {
 	int a = 8 , b = 4 ;
-	
+	cout << "a = 8 | b = 4" << endl ;
+	cout << endl ;
 	cout << "Addition result: " << ( a + b ) << endl ;
 	cout << "Subtraction result: " << ( a - b ) << endl ;
 	cout << "Multiplication result: " << ( a * b ) << endl ;
