e3487279879574a6205454c5723d8bb16bc04f95
Michael Smith <smithmichael130@gmail.com>
2014-02-21 21:08:35 +0000

    Added new file

---
+++ comparison/comparison.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+	int nil = 0, num = 0, max = 1 ; char cap = 'A', low = 'a' ;
+	cout << "Equality comparisons: " ;
+	cout << "(0 == 0) " << ( nil == num ) << "(true)" ;
+	cout << "(A == a) " << ( cap == low ) << "(false)" ;
+	
+	cout << endl << "Inequality comparison: " ;
+	cout << "(0 != 1) " << ( nil != max ) << "(true)" ;
+	
+	cout << endl << "Greater comparison: " ;
+	cout << "(0 > 1) " << ( nil > max ) << "(false)" ;
+	
+	cout << endl << "Lesser comparison: " ;
+	cout << "(0 < 1) " << ( nil < max ) << "(true)" ;
+	
+	return 0 ;
+}
