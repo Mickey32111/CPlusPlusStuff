49cc3eebf16e26889b909c1027185b8daf757879
Michael Smith <smithmichael130@gmail.com>
2014-02-21 20:42:25 +0000

    Added new file

---
+++ arithmetic/arithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ; 
+
+int main()
+{
+	int a = 8 , b = 4 ;
+	
+	cout << "Addition result: " << ( a + b ) << endl ;
+	cout << "Subtraction result: " << ( a - b ) << endl ;
+	cout << "Multiplication result: " << ( a * b ) << endl ;
+	cout << "Division result: " << ( a / b ) << endl ;
+	cout << "Modulus result: " << ( a % b ) << endl ;
+	
+	return 0 ;
+}
