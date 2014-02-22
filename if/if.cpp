ae9a64a8941972329d88e2ed0761eb03b3dbc3d3
Michael Smith <smithmichael130@gmail.com>
2014-02-22 19:13:27 +0000

    Added new file

---
+++ if/if.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+	int num = 8 ;
+	char letter = 'A' ;
+	
+	cout << "Number is 8" << endl << endl ;
+
+	if ( num > 5 )
+		{ cout << "Number more than five" << endl ; }
+	else
+		{ cout << "Number is five or less" << endl ; }
+		
+
+	return 0 ;
+}
