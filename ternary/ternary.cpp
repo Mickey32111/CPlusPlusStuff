e8df214c2581f4f8b997dc8032074a5023f2af67
Michael Smith <smithmichael130@gmail.com>
2014-02-22 18:59:40 +0000

    Added indent to main function

--- ternary/ternary.cpp
+++ ternary/ternary.cpp
@@ -3,20 +3,20 @@ using namespace std ;
 
 int main()
 {
-int a, b, max ;
-a = 1, b = 2 ;
-cout << "a = 1  |  b = 2" << endl << endl ;
+	int a, b, max ;
+	a = 1, b = 2 ;
+	cout << "a = 1  |  b = 2" << endl << endl ;
 
-cout << "Variable <a>: " ;
-cout << (( a != 1 ) ? "does not equal one, " : "does equal one, " ) ;
-cout << (( a & 2 != 0 ) ? "and is odd." : "and is even." ) ;
+	cout << "Variable <a>: " ;
+	cout << (( a != 1 ) ? "does not equal one, " : "does equal one, " ) ;
+	cout << (( a & 2 != 0 ) ? "and is odd." : "and is even." ) ;
 
-cout << endl << "Variable <b>: " ;
-cout << (( b != 1 ) ? "does not equal one, " : "does equal one, " ) ;
-cout << (( b & 2 != 0 ) ? "and is odd." : "and is even." ) ;
+	cout << endl << "Variable <b>: " ;
+	cout << (( b != 1 ) ? "does not equal one, " : "does equal one, " ) ;
+	cout << (( b & 2 != 0 ) ? "and is odd." : "and is even." ) ;
 
-max = ( a > b ) ? a : b ;
-cout << endl << "Greater value is: " << max << endl ; 
+	max = ( a > b ) ? a : b ;
+	cout << endl << "Greater value is: " << max << endl ; 
 
 return 0 ;
 }
